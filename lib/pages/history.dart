284128cb45e97ce8788b426f9aae8809e738181f
atech-01 <developeratech01@gmail.com>
2025-04-27 20:35:03 +0100

    change name again to atopup

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -5,6 +5,15 @@ class History extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        decoration: BoxDecoration(
+          color: Colors.white54,
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Center(child: Text("Coming Soon....")),
+      ),
+    );
   }
 }
